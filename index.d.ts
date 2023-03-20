0d60df35ec5a1ef31203348f42515d923c390926
Mostafa Talaat <mostafatalaat770@gmail.com>
2023-03-20 16:42:37 +0200

    feat: add WebView types
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -21,11 +21,52 @@ export interface BonuzJWTPayload extends JWTPayload, UserDetail {
 	walletAddress: string;
 }
 
-export type PostedMessage = {
-  bonuzToken: string;
-  name: string;
-  publicAddress: string;
-  privateKey: string;
-  metadataNonce: string;
-  typeOfUser: string;
+export type authenticationPayload = {
+	bonuzToken: string;
+	walletAddress: string;
+	privateKey: string;
+};
+
+declare enum WebViewMessageType {
+	ACTION = "action",
+	DATA = "data",
 }
+
+declare enum WebViewActions {
+	WALLET_CONNECT = "walletConnect",
+}
+
+declare enum WebViewDataNames {
+	AUTHENTICATION = "authentication",
+}
+
+interface WebViewMessageActionType<target extends string = string> {
+	target: target;
+}
+
+interface WebViewMessageDataType<
+	name extends string = string,
+	data extends object = object
+> {
+	name: name;
+	data: data;
+}
+
+export interface WebViewAuthenticationPayload
+	extends WebViewMessageDataType<
+		WebViewDataNames.AUTHENTICATION,
+		authenticationPayload & UserDetail
+	> {}
+
+export interface WebViewWalletConnectAction
+	extends WebViewMessageActionType<WebViewActions.WALLET_CONNECT> {}
+export interface WebViewMessageAction {
+	type: WebViewMessageType.ACTION;
+	payload: WebViewWalletConnectAction;
+}
+export interface WebviewMessageData {
+	type: WebViewMessageType.DATA;
+	payload: WebViewAuthenticationPayload;
+}
+
+export type WebViewMessage = WebViewMessageAction | WebviewMessageData;
